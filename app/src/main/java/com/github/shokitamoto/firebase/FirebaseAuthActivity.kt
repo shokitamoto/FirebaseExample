5c5e45797556d7e989cfe008eba02347115ac755
shokitamoto <kitamoto.ambitious@gmail.com>
2021-09-23 19:44:45 +0900

    9/23 エラーメッセージ： Execution failed for task ':app:dataBindingGenBaseClassesDebug'. > Found data binding error(s):

--- app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
+++ app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
@@ -12,6 +12,8 @@ class FirebaseAuthActivity: AppCompatActivity() {
 
 //    private var loginRegisterType = LoginRegisterType.LOGIN
 
+
+
     private lateinit var binding: ActivityFirebaseAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
