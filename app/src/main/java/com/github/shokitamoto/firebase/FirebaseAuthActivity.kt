8b426171e4adb564b29d2f393d4f7f19166c8ebf
shokitamoto <kitamoto.ambitious@gmail.com>
2021-09-28 19:21:17 +0900

    9/28 ログイン成功失敗による画面遷移がしたい

--- app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
+++ app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
@@ -2,7 +2,9 @@ package com.github.shokitamoto.firebase
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.github.shokitamoto.firebase.databinding.ActivityFirebaseAuthBinding
@@ -21,44 +23,108 @@ class FirebaseAuthActivity: AppCompatActivity() {
     }
 
     private fun initialize() {
+        binding.apply {
+            mailPasswordView.visibility = View.VISIBLE
+            loginButton.visibility = View.VISIBLE
+            registerSendButton.visibility = View.INVISIBLE
+            forgetPasswordSendButton.visibility = View.INVISIBLE
+            registerForgetPasswordView.visibility = View.VISIBLE
+        }
         initLayout()
     }
 
     private fun initLayout() {
         initClick()
-        updateView()
+//        updateView()
     }
 
     private fun initClick() {
         binding.apply {
             loginButton.setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-            forgetPasswordButton.setOnClickListener {
-                loginView.visibility = View.INVISIBLE
-                registerView.visibility = View.INVISIBLE
-                forgetPasswordView.visibility = View.VISIBLE
+                // todo: ここでログイン処理を書く
+                login()
+
             }
             registerButton.setOnClickListener {
-                loginView.visibility = View.INVISIBLE
-                registerView.visibility = View.INVISIBLE
-                forgetPasswordView.visibility = View.VISIBLE
-            }
-            registerSendButton.setOnClickListener {
+                mailPasswordView.visibility = View.VISIBLE
+                loginButton.visibility = View.INVISIBLE
+                registerSendButton.visibility = View.VISIBLE
+                forgetPasswordSendButton.visibility = View.INVISIBLE
+                registerForgetPasswordView.visibility = View.INVISIBLE
 
             }
-            forgetPasswordSendButton.setOnClickListener {
+            forgetPasswordButton.setOnClickListener {
+                mailView.visibility = View.VISIBLE
+                passwordView.visibility = View.INVISIBLE
+                loginButton.visibility = View.INVISIBLE
+                registerSendButton.visibility = View.INVISIBLE
+                forgetPasswordSendButton.visibility = View.VISIBLE
+                registerForgetPasswordView.visibility = View.INVISIBLE
 
             }
+
+//            registerSendButton.setOnClickListener {
+//                // todo: ここでログイン処理を書く
+//                login()
+//
+//            }
+//            forgetPasswordSendButton.setOnClickListener {
+//                // todo: ここでパスワード再設定の処理を書く
+//                forgetPassword()
+//            }
         }
     }
 
     private fun updateView() {
         var user = FirebaseAuth.getInstance().currentUser
         var isLogin = (user != null) // userがnullでなければ、isLogin = true
+        if (isLogin) loginSuccess() else loginFailure()
+    }
+    private fun loginSuccess() {
+        val intent = Intent(this@FirebaseAuthActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun loginFailure() {
+
     }
 
+    private fun login() {
+        val pair = getPair()
+        pair?.also {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(it.first, it.second)
+                .addOnCompleteListener {
+//                    addOnCompleteListener.d("login task:$it")
+                    toast(if (it.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
+                    updateView()
+                }
+        }
+    }
+
+    private fun forgetPassword() {
+
+    }
+    private fun getPair(isOnlyMailAddress: Boolean = false): Pair<String,String>? {
+        val mailAddress = getMailAddress()
+        if (mailAddress.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mailAddress).matches()){
+            toast(R.string.firebase_auth_warn_mail)
+            return null
+        }
+        val password = getPassword()
+        if (!isOnlyMailAddress && (password.isEmpty() || password.length < 8)) {
+            toast(R.string.firebase_auth_warn_password)
+            return null
+        }
+        return Pair(mailAddress, password)
+    }
+
+
+
+    private fun getMailAddress(): String = binding.mailEditText.text.toString()
+    private fun getPassword(): String = binding.passwordEditText.text.toString()
+
+    private fun toast(textId: Int) {
+        Toast.makeText(this, textId, Toast.LENGTH_SHORT).show()
+    }
     enum class LoginRegisterType {
         LOGIN, REGISTER, FORGET_MAIL
     }
