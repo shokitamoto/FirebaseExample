c85a5bc7569f4bee74fd60702fe99c01b278c9be
shokitamoto <kitamoto.ambitious@gmail.com>
2021-09-28 22:33:26 +0900

    9/28 ■やりたいこと 登録画面でメールアドレスとパスワードを正しく入力することで、 MainActivityに遷移。
    ■現在の挙動
    ・登録画面でメールアドレスかパスワードを正しく入力しない
    　-> toast「パスワードを正しく入力してください」と表示。
    ・登録画面でメールアドレスとパスワードを正しく入力
     -> toastは表示されないが、MainActivityに遷移しない。

--- app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
+++ app/src/main/java/com/github/shokitamoto/firebase/FirebaseAuthActivity.kt
@@ -31,28 +31,35 @@ class FirebaseAuthActivity: AppCompatActivity() {
             registerForgetPasswordView.visibility = View.VISIBLE
         }
         initLayout()
+        // ログインしている確認
+        if (FirebaseAuth.getInstance().currentUser != null) { // nullじゃない場合はログイン
+            moveMainActivity()
+        }
+
+        // todo:+α
+        // ログインしていない時loginFragmentを出す
+        // navigationを使ってActivity×1とFragmentで表現する
+        // Jetpack Composeの使用
     }
 
     private fun initLayout() {
         initClick()
-//        updateView()
     }
 
     private fun initClick() {
         binding.apply {
             loginButton.setOnClickListener {
-                // todo: ここでログイン処理を書く
                 login()
-
             }
+
             registerButton.setOnClickListener {
                 mailPasswordView.visibility = View.VISIBLE
                 loginButton.visibility = View.INVISIBLE
                 registerSendButton.visibility = View.VISIBLE
                 forgetPasswordSendButton.visibility = View.INVISIBLE
                 registerForgetPasswordView.visibility = View.INVISIBLE
-
             }
+
             forgetPasswordButton.setOnClickListener {
                 mailView.visibility = View.VISIBLE
                 passwordView.visibility = View.INVISIBLE
@@ -60,47 +67,46 @@ class FirebaseAuthActivity: AppCompatActivity() {
                 registerSendButton.visibility = View.INVISIBLE
                 forgetPasswordSendButton.visibility = View.VISIBLE
                 registerForgetPasswordView.visibility = View.INVISIBLE
-
             }
 
-//            registerSendButton.setOnClickListener {
-//                // todo: ここでログイン処理を書く
-//                login()
-//
-//            }
-//            forgetPasswordSendButton.setOnClickListener {
-//                // todo: ここでパスワード再設定の処理を書く
-//                forgetPassword()
-//            }
+            registerSendButton.setOnClickListener {
+                login()
+            }
+
+            forgetPasswordSendButton.setOnClickListener {
+                // todo: ここでパスワード再設定の処理を書く
+                forgetPassword()
+            }
         }
     }
 
-    private fun updateView() {
-        var user = FirebaseAuth.getInstance().currentUser
-        var isLogin = (user != null) // userがnullでなければ、isLogin = true
-        if (isLogin) loginSuccess() else loginFailure()
-    }
-    private fun loginSuccess() {
+//    private fun updateView() {
+//        var user = FirebaseAuth.getInstance().currentUser
+//        var isLogin = (user != null) // userがnullでなければ、isLogin = true
+//        if (isLogin) moveMainActivity() else
+//    }
+
+    private fun moveMainActivity() {
         val intent = Intent(this@FirebaseAuthActivity, MainActivity::class.java)
         startActivity(intent)
     }
-    private fun loginFailure() {
 
-    }
 
     private fun login() {
         val pair = getPair()
         pair?.also {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(it.first, it.second)
-                .addOnCompleteListener {
-//                    addOnCompleteListener.d("login task:$it")
-                    toast(if (it.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
-                    updateView()
+                .addOnCompleteListener { task ->
+//                    Timber.d("login task:$it") ログだし。作って学ぶ参照
+                    if (!task.isSuccessful) { // 失敗した時
+                        // todo:失敗した時の処理。原因がわかるならユーザーに教える
+                        return@addOnCompleteListener
+                    }
+                    moveMainActivity()
+                    toast(if (task.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
                 }
         }
     }
-
-    
 
     private fun forgetPassword() {
 
@@ -119,15 +125,10 @@ class FirebaseAuthActivity: AppCompatActivity() {
         return Pair(mailAddress, password)
     }
 
-
-
     private fun getMailAddress(): String = binding.mailEditText.text.toString()
     private fun getPassword(): String = binding.passwordEditText.text.toString()
 
     private fun toast(textId: Int) {
         Toast.makeText(this, textId, Toast.LENGTH_SHORT).show()
-    }
-    enum class LoginRegisterType {
-        LOGIN, REGISTER, FORGET_MAIL
     }
 }
